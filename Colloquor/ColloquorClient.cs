707396cca22a3934729d02d781a72bf08bac3a25
Ignacio Tampe <Igtampe@outlook.com>
2020-07-03 19:25:28 -0400

    Add project files.

---
+++ Colloquor/ColloquorClient.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using Switchboard;
+
+namespace Colloquor {
+    class ColloquorClient:SwitchboardClient {
+
+        private String Username;
+        private String Password;
+
+        /// <summary>Holds the welcome message so that the head can read it after noticing a connection was successfull.</summary>
+        public String WelcomeHolder;
+
+        public enum CQUORConnectionResult:int {
+            NOCONNECT = 0,
+            LOGININVALID = 1,
+            LOGINALREADY = 2,
+            LOGINOTHERLOCALE = 3,
+            LOGINOTHER = 4,
+            NOCOLLOQUOR = 5,
+            NOPERMISSION = 6,
+            SUCCESS = 7
+        }
+
+        public enum CQUORJoinResult:int {
+            ALREADY = 0,
+            NOTFOUND = 1,
+            NEEDPASS = 2,
+            WRONGPASS = 3,
+            SUCCESS = 4
+        }
+
+        public ColloquorClient(String IP,int Port,String Username,String Password) : base(IP,Port) {
+            this.Username = Username;
+            this.Password = Password;
+        }
+
+        /// <summary>Connects to the Colloquor Server</summary>
+        public new CQUORConnectionResult Connect() {
+            
+            //Connect to the server
+            if(!base.Connect()) { return CQUORConnectionResult.NOCONNECT; }
+            
+            //Login to the server
+            switch(Login(Username,Password)) {
+                case LoginResult.SUCCESS:
+                    break;
+                case LoginResult.INVALID:
+                    return CQUORConnectionResult.LOGININVALID;
+                case LoginResult.ALREADY:
+                   return  CQUORConnectionResult.LOGINALREADY;
+                case LoginResult.OTHERLOCALE:
+                    return CQUORConnectionResult.LOGINOTHERLOCALE;
+                default:
+                    return CQUORConnectionResult.LOGINOTHER;
+            }
+
+            //Check for Colloquor
+            switch(SendReceive("CQUOR PING")) {
+                case "PONG":
+                    return CQUORConnectionResult.SUCCESS;
+                case "NOEXECUTE":
+                    return CQUORConnectionResult.NOPERMISSION;
+                default:
+                    return CQUORConnectionResult.NOCOLLOQUOR;
+
+            }
+
+        }
+
+        /// <summary>Gets all channels on the colloquor server</summary>
+        /// <returns>A dictionary, where the key is the Channel's Name, and the value is wether or not the channel has a password.</returns>
+        public Dictionary<String,Boolean> GetChannels() {
+            //no need to check if the server is connected or not. Send/Receive throws it already.
+
+            //Create the dictionary
+            Dictionary<String,Boolean> Channels = new Dictionary<string,bool>();
+            
+            String[] AllChannels = SendReceive("CQUOR ListChannels").Split(',');
+
+            foreach(String Channel in AllChannels) {
+                String[] ChannelSplit = Channel.Split(':');
+                if(ChannelSplit.Length == 2) { Channels.Add(ChannelSplit[0],bool.Parse(ChannelSplit[1])); }
+            }
+
+            return Channels;
+        
+        }
+
+        public String SendMessage(String Message) {
+            String Reply = SendReceive("CQUOR SEND " + Message);
+            if(Reply == "NOT CONNECTED") { throw new InvalidOperationException("User has not connected to a channel"); }
+            return Reply;
+
+        }
+
+        public String RequestMessage() {
+            String Reply = SendReceive("CQUOR REQUEST");
+            if(Reply == "NOT CONNECTED") { throw new InvalidOperationException("User has not connected to a channel"); }
+            return Reply;
+        }
+
+        public Boolean Leave() {
+            String Reply = SendReceive("CQUOR LEAVE");
+            if(Reply == "NOT CONNECTED") { throw new InvalidOperationException("User has not connected to a channel"); }
+            return true;
+        }
+
+        public CQUORJoinResult Join(String Channel) {return Join(Channel,"");}
+
+        public CQUORJoinResult Join(String Channel,String Password) {
+            String Holder = SendReceive("CQUOR JOIN " + Channel + " " +Password);
+            switch(Holder) {
+                case "ALREADY CONNECTED":
+                    return CQUORJoinResult.ALREADY;
+                case "NOT FOUND":
+                    return CQUORJoinResult.NOTFOUND;
+                case "NEEDS PASS":
+                    return CQUORJoinResult.NEEDPASS;
+                case "WRONG PASS":
+                    return CQUORJoinResult.WRONGPASS;
+                default:
+                    WelcomeHolder = Holder;
+                    return CQUORJoinResult.SUCCESS;
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ LoginForm.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ColloquorClient {
+    partial class LoginForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(204, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 365);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(14, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 87);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server Info";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(14, 105);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(184, 94);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "User Info";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Localhost";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(60, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(118, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "909";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(67, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(111, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(67, 60);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(111, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(117, 354);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 406);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "LoginForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
